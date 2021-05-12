b57f523880c3c3a0db53a932c058b30fd6b15c0c
brunoBMonteiro <brunobilheri@hotmail.com>
2021-05-12 18:15:11 -0300

    construindo estilização do app

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { StyleSheet, View, Image} from 'react-native';
+import { 
+  StyleSheet, 
+  View,
+  Text, 
+  Image,
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
 
 import foto from './assets/WhatsAppImage.jpeg';
 
 export default function App() {
   return (
     <>
-      <View style={styles.page}>
-        <View>
-          <Image source={foto} style={styles.foto} />
+      <SafeAreaView>
+        <View style={styles.page}>
+          <View style={styles.container_header}> 
+            <Image source={foto} style={styles.profilePicture} />
+            <Text style={styles.name}>Bruno Bilheri Monteiro</Text>
+
+            <View style={styles.information}>
+              
+            </View>
+
+            <View style={styles.footer}> 
+              <View style={styles.socialMidia}> 
+                <Text>Linkedin</Text>
+                <Text>Github</Text>
+                <Text>WhatsApp</Text>
+              </View>
+            </View>
+          </View>
+
         </View>
-      </View>
+
+
+        <StatusBar />
+      </SafeAreaView>
     </>
   );
 }
@@ -20,9 +46,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E7E7E7',
   },
-  foto: {
-    width: 250,
-    height: 250,
+  container_header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  profilePicture: {
+    width: 150,
+    height: 150,
+    marginTop: 200,
     borderRadius: 125,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginTop: 10,
+  },
+  socialMidia: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
